af1a13250074667684d013663b75d9e1fba9bd1c
stmSi <stm1998sithumyo@gmail.com>
2024-06-28 14:55:56 +0630

    fix: unit test failure

--- packages/implementations-lib/test/unit/transfers/mongo_transfers_repo.test.ts
+++ packages/implementations-lib/test/unit/transfers/mongo_transfers_repo.test.ts
@@ -501,8 +501,8 @@ describe("Implementations - Mongo Transfers Repo Unit Tests", () => {
                 { transferType: 'transferType' },
                 { payerIdType: 'payerIdType' },
                 { payeeIdType: 'payeeIdType' },
-                { amount: '0.0' },
                 { bulkTransferId: { $regex: 'bulkTransferId', $options: 'i' } },
+                { amount: '0.0' },
             ],
         },{
             limit: 100, 
